4368d8a482e4382ce2ec736144a3838d827d4d81
unknown <nolan.beihl@gmail.com>
2021-10-13 09:59:38 -0600

    Developed weapons and Robots

---
+++ .vscode/main.py
@@ -0,0 +1,17 @@
+from weapon import Weapon
+from robot import Robot
+
+weapon1 = Weapon('sword', 10)
+weapon2 = Weapon('gun', 25)
+weapon3 = Weapon('laser', 100)
+weapon4 = Weapon('love', 5)
+
+robot1 = Robot('Wallie', 200, weapon4)
+robot2 = Robot('Johhn 5', 200, weapon3)
+robot3 = Robot('T1000', 500, weapon2)
+
+
+print(robot1)
+
+
+
