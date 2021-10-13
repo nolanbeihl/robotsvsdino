bcd867cb16cc0164cc7a5898b4bbf81185c63715
unknown <nolan.beihl@gmail.com>
2021-10-13 09:29:22 -0600

    Completed the foundation of all classes

---
+++ .vscode/battlefield.py
@@ -0,0 +1,30 @@
+class Battlefield:
+    def __init__(self, fleet, herd):
+        self.fleet = fleet
+        self.herd = herd
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def dino_turn(self,dinosaur):
+        pass
+
+    def robo_turn(self, robot):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
+
+    
---
+++ .vscode/weapon.py
@@ -0,0 +1,11 @@
+class Weapon:
+    def __init__(self, name, attack_power):
+        self.name = ''
+        self.attack_power = int
+
+    def name(self, weapon_name):
+        self.name = weapon_name
+
+    def attack_power(self, power):
+        self.attack_power = power
+
